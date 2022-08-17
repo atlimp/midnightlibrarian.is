d65b8838c700c2f33075bc86de8aca502a9ecd6d
Atli Marcher Palsson <atlimarcher@gmail.com>
2022-08-17 21:27:26 +0000

    fix 404 middleware

--- backend/src/middleware/notfoundmiddleware.ts
+++ backend/src/middleware/notfoundmiddleware.ts
@@ -2,5 +2,5 @@ import { NextFunction, Request, Response } from 'express';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function notFoundMiddleware(req: Request, res: Response, next: NextFunction) {
-    res.redirect('/404');
+    return res.status(404).json({ status: 404, message: "Resource not found" });
 }
