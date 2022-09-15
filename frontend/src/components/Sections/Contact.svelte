ebdbc21173487dfbf260c1219bc76e8438f9894f
Atli Marcher Palsson <atlimarcher@gmail.com>
2022-09-15 19:48:29 +0000

    update email address for contact info again

--- frontend/src/components/Sections/Contact.svelte
+++ frontend/src/components/Sections/Contact.svelte
@@ -1,6 +1,6 @@
 <div class="grid place-items-center p-8 blur-2xs">
     <h1 class="text-center pb-32 pt-12 text-lg lg:text-3xl">
         Want to get in contact with us?<br/><br/>
-        Send us an email at <a class="transition-colors underline hover:text-orange" href="mailto:info@midnightlibrarian.is">midnightlibrarianband@gmail.com</a>  
+        Send us an email at <a class="transition-colors underline hover:text-orange" href="mailto:info@midnightlibrarian.is">info@midnightlibrarian.is</a>  
     </h1>
 </div>
