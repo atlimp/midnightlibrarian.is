47cbfa778fdf61315a244a68115bd0a7fbd95ffc
Atli Marcher Palsson <atlimarcher@gmail.com>
2022-09-23 21:18:17 +0000

    add flex-wrap to socials

--- frontend/src/components/Sections/Socials.svelte
+++ frontend/src/components/Sections/Socials.svelte
@@ -8,9 +8,9 @@
     <h1 class="text-center py-32 text-lg lg:text-3xl pb-12 pt-24">
         Find us on social media
     </h1>
-    <div class="flex gap-4 lg:gap-8">
+    <div class="flex gap-4 lg:gap-8 flex-wrap justify-center">
         {#each links as { site, svg, link }}
-            <a href={link}>
+            <a href={link} class="basis-1/5 sm:basis-0">
                 <Icon style="h-12 w-12 sm:h-16 sm:w-16 xl:h-20 xl:w-20 drop-shadow-icon blur-2xs fill-white cursor-pointer hover:drop-shadow-icon-orange hover:fill-orange focus:outline-0 transition-svg duration-75" name={site} svg={svg} />
             </a>
         {/each}
